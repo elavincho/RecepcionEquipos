dbba5928a02100ad54f43eb1d51792cfa4d50c40
elavincho <elavincho@gmail.com>
2025-02-08 14:35:48 -0300

    Agrego Home y el controlador

--- src/main/java/com/developers/recepcionEquipos/controlador/UsuarioController.java
+++ src/main/java/com/developers/recepcionEquipos/controlador/UsuarioController.java
@@ -1,5 +1,15 @@
 package com.developers.recepcionEquipos.controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class UsuarioController {
     
+    @GetMapping("")
+    public String home() {
+        return "usuario/home";
+    }
 }
